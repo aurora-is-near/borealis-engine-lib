9e5781a2feb0923c0fccbbb50a75a3f3a4a29357
Michael Birch <michael.birch@aurora.dev>
2023-08-01 15:07:48 +0200

    Doc comment for get_height_from_log

--- etc/integration-tests/src/parse_logs.rs
+++ etc/integration-tests/src/parse_logs.rs
@@ -3,6 +3,9 @@
 use crate::ansi_utils;
 use tokio::io::{AsyncBufReadExt, AsyncRead, BufReader};
 
+/// Extracts the block height from a nearcore stats log line. Exmaple:
+/// 2023-08-01T08:26:02.869336Z  INFO stats: #      52 24LGJniHqZjL4nhd55ocngvTofG8jyTKQNCgixcTsJTV Validator | 4 validators 3 peers ⬇ 4.92 kB/s ⬆ 4.80 kB/s 1.90 bps 0 gas/s CPU: 0%, Mem: 59.9 MB
+/// Notably there are 8 characters availble for the block height after the '#' in the line.
 fn get_height_from_log(line: &str) -> anyhow::Result<u32> {
     let index = line
         .find('#')
