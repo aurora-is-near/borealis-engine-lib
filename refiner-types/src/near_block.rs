c038c76a3c0067eee98eb04230bd63e8f60550fa
Julian Kulesh <julian.kulesh@aurora.dev>
2024-04-04 11:45:31 +0800

    chore: experimental conversion from indexer-types

--- refiner-types/src/near_block.rs
+++ refiner-types/src/near_block.rs
@@ -1,4 +1,4 @@
-use near_crypto::Signature;
+use near_crypto::{Secp256K1Signature, Signature};
 use near_primitives::challenge::ChallengesResult;
 use near_primitives::hash::CryptoHash;
 use near_primitives::serialize::dec_format;
@@ -7,16 +7,25 @@ use near_primitives::types::{
 };
 use near_primitives::views;
 use near_primitives::views::validator_stake_view::ValidatorStakeView;
-use near_primitives::views::{
-    StateChangeCauseView, StateChangeValueView, StateChangeWithCauseView,
-};
+use near_primitives::views::{StateChangeCauseView, StateChangeValueView, StateChangeWithCauseView, ValidatorStakeViewV1};
 use serde::{Deserialize, Serialize};
+
+use near_lake_framework::near_indexer_primitives;
 
 /// Resulting struct represents block with chunks
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct NEARBlock {
     pub block: BlockView,
     pub shards: Vec<Shard>,
+}
+
+impl From<near_indexer_primitives::StreamerMessage> for NEARBlock {
+    fn from(block: near_indexer_primitives::StreamerMessage) -> Self {
+        Self {
+            block: BlockView::from(block.block),
+            shards: block.shards.into_iter().map(Shard::from).collect(),
+        }
+    }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -30,6 +39,15 @@ impl From<views::BlockView> for BlockView {
         Self {
             author: view.author,
             header: view.header.into(),
+        }
+    }
+}
+
+impl From<near_indexer_primitives::views::BlockView> for BlockView {
+    fn from(view: near_indexer_primitives::views::BlockView) -> Self {
+        Self {
+            author: view.author,
+            header: view.header,
         }
     }
 }
@@ -138,6 +156,43 @@ impl From<views::BlockHeaderView> for IndexerBlockHeaderView {
             approvals,
             signature,
             latest_protocol_version,
+        }
+    }
+}
+
+impl From<near_indexer_primitives::views::BlockHeaderView> for IndexerBlockHeaderView {
+    fn from(header: near_indexer_primitives::views::BlockHeaderView) -> Self {
+        Self {
+            height: header.height,
+            prev_height: header.prev_height,
+            epoch_id: header.epoch_id.into_borealis_types(),
+            next_epoch_id: header.next_epoch_id.into_borealis_types(),
+            hash: header.hash.into_borealis_types(),
+            prev_hash: header.prev_hash.into_borealis_types(),
+            prev_state_root: header.prev_state_root.into_borealis_types(),
+            chunk_receipts_root: header.chunk_receipts_root.into_borealis_types(),
+            chunk_headers_root: header.chunk_headers_root.into_borealis_types(),
+            chunk_tx_root: header.chunk_tx_root.into_borealis_types(),
+            outcome_root: header.outcome_root.into_borealis_types(),
+            chunks_included: header.chunks_included,
+            challenges_root: header.challenges_root.into_borealis_types(),
+            timestamp: header.timestamp,
+            timestamp_nanosec: header.timestamp_nanosec,
+            random_value: header.random_value.into_borealis_types(),
+            validator_proposals: header.validator_proposals,
+            chunk_mask: header.chunk_mask,
+            gas_price: header.gas_price,
+            block_ordinal: header.block_ordinal,
+            total_supply: header.total_supply,
+            challenges_result: header.challenges_result,
+            last_final_block: header.last_final_block.into_borealis_types(),
+            last_ds_final_block: header.last_ds_final_block.into_borealis_types(),
+            next_bp_hash: header.next_bp_hash.into_borealis_types(),
+            block_merkle_root: header.block_merkle_root.into_borealis_types(),
+            epoch_sync_data_hash: header.epoch_sync_data_hash.map(|v| v.into_borealis_types()),
+            approvals: header.approvals.into_iter().map(|v| v.map(|v| v.into_borealis_types())).collect(),
+            signature: header.signature,
+            latest_protocol_version: header.latest_protocol_version,
         }
     }
 }
@@ -325,6 +380,21 @@ impl Clone for Shard {
     }
 }
 
+impl From<near_indexer_primitives::IndexerShard> for Shard {
+    fn from(shard: near_indexer_primitives::IndexerShard) -> Self {
+        Self {
+            shard_id: shard.shard_id,
+            chunk: shard.chunk.map(ChunkView::from),
+            receipt_execution_outcomes: shard
+                .receipt_execution_outcomes
+                .into_iter()
+                .map(ExecutionOutcomeWithReceipt::from)
+                .collect(),
+            state_changes: shard.state_changes,
+        }
+    }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ChunkView {
     pub author: AccountId,
@@ -333,10 +403,30 @@ pub struct ChunkView {
     pub receipts: Vec<views::ReceiptView>,
 }
 
+impl From<near_indexer_primitives::IndexerChunkView> for ChunkView {
+    fn from(chunk: near_indexer_primitives::IndexerChunkView) -> Self {
+        Self {
+            author: chunk.author,
+            header: chunk.header.into(),
+            transactions: chunk.transactions.into_iter().map(TransactionWithOutcome::from).collect(),
+            receipts: chunk.receipts.into(),
+        }
+    }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct TransactionWithOutcome {
     pub transaction: views::SignedTransactionView,
     pub outcome: ExecutionOutcomeWithOptionalReceipt,
+}
+
+impl From<near_indexer_primitives::IndexerTransactionWithOutcome> for TransactionWithOutcome {
+    fn from(transaction: near_indexer_primitives::IndexerTransactionWithOutcome) -> Self {
+        Self {
+            transaction: transaction.transaction.into(),
+            outcome: transaction.outcome.into(),
+        }
+    }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -345,8 +435,46 @@ pub struct ExecutionOutcomeWithOptionalReceipt {
     pub receipt: Option<views::ReceiptView>,
 }
 
+impl From<near_indexer_primitives::IndexerExecutionOutcomeWithOptionalReceipt> for ExecutionOutcomeWithOptionalReceipt {
+    fn from(outcome: near_indexer_primitives::IndexerExecutionOutcomeWithOptionalReceipt) -> Self {
+        Self {
+            execution_outcome: outcome.execution_outcome.into(),
+            receipt: outcome.receipt.into(),
+        }
+    }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ExecutionOutcomeWithReceipt {
     pub execution_outcome: views::ExecutionOutcomeWithIdView,
     pub receipt: views::ReceiptView,
+}
+
+trait IntoBorealisTypes<T> {
+    fn into_borealis_types(self) -> T;
+}
+
+impl IntoBorealisTypes<CryptoHash> for near_indexer_primitives::CryptoHash {
+    fn into_borealis_types(self) -> CryptoHash {
+        CryptoHash(self.0)
+    }
+}
+
+impl IntoBorealisTypes<Signature> for near_crypto::signature::Signature {
+    fn into_borealis_types(self) -> Signature {
+        match self {
+            near_crypto::signature::Signature::ED25519(signature) => Signature::ED25519(signature),
+            near_crypto::signature::Signature::SECP256K1(signature) => Signature::SECP256K1(signature),
+        }
+    }
+}
+
+impl IntoBorealisTypes<ValidatorStakeView> for near_indexer_primitives::views::validator_stake_view::ValidatorStakeView {
+    fn into_borealis_types(self) -> ValidatorStakeView {
+        match self {
+            near_indexer_primitives::views::validator_stake_view::ValidatorStakeView::V1(validator_stake_view_v1) => {
+                ValidatorStakeView::V1(validator_stake_view_v1.into_borealis_types())
+            }
+        }
+    }
 }
