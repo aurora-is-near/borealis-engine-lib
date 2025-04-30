0b7a8f087043b0dbf8774f481f08b4a9fb51b95d
Alexander Borodulya <alexander.borodulya@gmail.com>
2025-04-30 16:52:22 +0300

    Fix fn order

--- refiner-lib/src/signal_handlers.rs
+++ refiner-lib/src/signal_handlers.rs
@@ -9,25 +9,60 @@ static SIGNAL_USR1: i32 = 10;
 static SIGNAL_USR2: i32 = 12;
 
 /// Tests a `signal_value` representing a system signal
-/// to ensure it matches one of the following values: 2 | 15 | 12 | 10 | 1
+/// to ensure it matches one of the following values: 1 2 3 10 12 15
 pub fn is_matching_signal() -> bool {
     let signal_value: SignalKind = SIGNAL.load(Ordering::SeqCst).into();
 
     match signal_value {
-        // SIGINT (2)
-        val if val == SignalKind::interrupt() => true,
-        // TERM (15)
-        val if val == SignalKind::terminate() => true,
-        // USR2 (12)
-        val if val == SignalKind::from_raw(SIGNAL_USR2) => true,
-        // USR1 (10)
-        val if val == SignalKind::from_raw(SIGNAL_USR1) => true,
         // SIGHUP (1)
         val if val == SignalKind::hangup() => true,
+        // SIGINT (2)
+        val if val == SignalKind::interrupt() => true,
         // SIGQUIT (3)
         val if val == SignalKind::quit() => true,
+        // USR1 (10)
+        val if val == SignalKind::from_raw(SIGNAL_USR1) => true,
+        // USR2 (12)
+        val if val == SignalKind::from_raw(SIGNAL_USR2) => true,
+        // TERM (15)
+        val if val == SignalKind::terminate() => true,
         _ => false,
     }
+}
+
+/// Creates a future that handles HUP signal and sends shutdown signal when triggered
+pub async fn handle_hup(shutdown_tx: tokio::sync::broadcast::Sender<()>) -> anyhow::Result<()> {
+    let sighup = SignalKind::hangup();
+    let mut hup_signal_stream = signal(sighup)?;
+    info!("Signal handler for SIGHUP installed");
+
+    hup_signal_stream.recv().await;
+
+    info!("Signal handler for SIGHUP triggered");
+    SIGNAL.store(sighup.into(), Ordering::SeqCst);
+    match shutdown_tx.send(()) {
+        Ok(_) => info!("Originated by SIGHUP shutdown signal sent successfully"),
+        Err(SendError(_)) => warn!("No active receivers for shutdown signal originated by SIGHUP"),
+    }
+
+    Ok(())
+}
+
+/// Creates a future that handles Ctrl+C and sends shutdown signal when triggered
+pub async fn handle_ctrl_c(shutdown_tx: tokio::sync::broadcast::Sender<()>) -> anyhow::Result<()> {
+    let sigint = SignalKind::interrupt();
+    info!("Signal handler for Ctrl-C installed");
+
+    tokio::signal::ctrl_c().await?;
+
+    info!("Signal handler for Ctrl-C triggered");
+    SIGNAL.store(sigint.into(), Ordering::SeqCst);
+    match shutdown_tx.send(()) {
+        Ok(_) => info!("Originated by Ctrl-C shutdown signal sent successfully"),
+        Err(SendError(_)) => warn!("No active receivers for shutdown signal originated by Ctrl-C"),
+    }
+
+    Ok(())
 }
 
 /// Creates a future that handles SIGQUIT signal and sends shutdown signal when triggered
@@ -100,83 +135,66 @@ pub async fn handle_term(shutdown_tx: tokio::sync::broadcast::Sender<()>) -> any
     Ok(())
 }
 
-/// Creates a future that handles HUP signal and sends shutdown signal when triggered
-pub async fn handle_hup(shutdown_tx: tokio::sync::broadcast::Sender<()>) -> anyhow::Result<()> {
-    let sighup = SignalKind::hangup();
-    let mut hup_signal_stream = signal(sighup)?;
-    info!("Signal handler for SIGHUP installed");
-
-    hup_signal_stream.recv().await;
-
-    info!("Signal handler for SIGHUP triggered");
-    SIGNAL.store(sighup.into(), Ordering::SeqCst);
-    match shutdown_tx.send(()) {
-        Ok(_) => info!("Originated by SIGHUP shutdown signal sent successfully"),
-        Err(SendError(_)) => warn!("No active receivers for shutdown signal originated by SIGHUP"),
-    }
-
-    Ok(())
-}
-
-/// Creates a future that handles Ctrl+C and sends shutdown signal when triggered
-pub async fn handle_ctrl_c(shutdown_tx: tokio::sync::broadcast::Sender<()>) -> anyhow::Result<()> {
-    let sigint = SignalKind::interrupt();
-    info!("Signal handler for Ctrl-C installed");
-
-    tokio::signal::ctrl_c().await?;
-
-    info!("Signal handler for Ctrl-C triggered");
-    SIGNAL.store(sigint.into(), Ordering::SeqCst);
-    match shutdown_tx.send(()) {
-        Ok(_) => info!("Originated by Ctrl-C shutdown signal sent successfully"),
-        Err(SendError(_)) => warn!("No active receivers for shutdown signal originated by Ctrl-C"),
-    }
-
-    Ok(())
-}
-
 /// Creates a future that handles all signals and sends shutdown signal when any is triggered
 pub async fn handle_all_signals(
     shutdown_tx: tokio::sync::broadcast::Sender<()>,
 ) -> anyhow::Result<()> {
     info!("Installing signal handlers");
 
-    let mut usr1 = Box::pin(handle_usr1(shutdown_tx.clone()));
-    let mut usr2 = Box::pin(handle_usr2(shutdown_tx.clone()));
-    let mut term = Box::pin(handle_term(shutdown_tx.clone()));
     let mut hup = Box::pin(handle_hup(shutdown_tx.clone()));
     let mut ctrl_c = Box::pin(handle_ctrl_c(shutdown_tx.clone()));
-    let mut quit = Box::pin(handle_quit(shutdown_tx));
+    let mut quit = Box::pin(handle_quit(shutdown_tx.clone()));
+    let mut usr1 = Box::pin(handle_usr1(shutdown_tx.clone()));
+    let mut usr2 = Box::pin(handle_usr2(shutdown_tx.clone()));
+    let mut term = Box::pin(handle_term(shutdown_tx));
 
-    tokio::select! {
-        result = &mut usr1 => {
-            if let Err(e) = result {
-                warn!("Error handling USR1 signal: {}", e);
-            }
-        }
-        result = &mut usr2 => {
-            if let Err(e) = result {
-                warn!("Error handling USR2 signal: {}", e);
-            }
-        }
-        result = &mut term => {
-            if let Err(e) = result {
-                warn!("Error handling TERM signal: {}", e);
-            }
-        }
+    let result = tokio::select! {
         result = &mut hup => {
             if let Err(e) = result {
                 warn!("Error handling HUP signal: {}", e);
+                Err(e)
+            } else {
+                Ok(())
             }
         }
         result = &mut ctrl_c => {
             if let Err(e) = result {
                 warn!("Error handling Ctrl-C signal: {}", e);
+                Err(e)
+            } else {
+                Ok(())
             }
         }
         result = &mut quit => {
             if let Err(e) = result {
                 warn!("Error handling QUIT signal: {}", e);
+                Err(e)
+            } else {
+                Ok(())
+            }
+        }
+        result = &mut usr1 => {
+            if let Err(e) = result {
+                warn!("Error handling USR1 signal: {}", e);
+                Err(e)
+            } else {
+                Ok(())
+            }
+        }
+        result = &mut usr2 => {
+            if let Err(e) = result {
+                warn!("Error handling USR2 signal: {}", e);
+                Err(e)
+            } else {
+                Ok(())
+            }
+        }
+        result = &mut term => {
+            if let Err(e) = result {
+                warn!("Error handling TERM signal: {}", e);
+                Err(e)
+            } else {
+                Ok(())
             }
         }
     };
@@ -184,7 +202,7 @@ pub async fn handle_all_signals(
     info!("Stopping actix system");
     actix::System::current().stop();
 
-    Ok(())
+    result
 }
 
 #[cfg(test)]
@@ -197,8 +215,16 @@ mod tests {
         SIGNAL.store(602437500, Ordering::SeqCst);
         assert!(!is_matching_signal());
 
+        // SIGHUP (1)
+        SIGNAL.store(SignalKind::hangup().into(), Ordering::SeqCst);
+        assert!(is_matching_signal());
+
         // SIGINT (2)
         SIGNAL.store(SignalKind::interrupt().into(), Ordering::SeqCst);
+        assert!(is_matching_signal());
+
+        // SIGQUIT (3)
+        SIGNAL.store(SignalKind::quit().into(), Ordering::SeqCst);
         assert!(is_matching_signal());
 
         // SIGUSR1 (10)
@@ -211,14 +237,6 @@ mod tests {
 
         // TERM (15)
         SIGNAL.store(SignalKind::terminate().into(), Ordering::SeqCst);
-        assert!(is_matching_signal());
-
-        // SIGHUP (1)
-        SIGNAL.store(SignalKind::hangup().into(), Ordering::SeqCst);
-        assert!(is_matching_signal());
-
-        // SIGQUIT (3)
-        SIGNAL.store(SignalKind::quit().into(), Ordering::SeqCst);
         assert!(is_matching_signal());
     }
 }
